7886b5a8a50fe33704f3485e9d35557eed68fe59
Francesco <francesco.savignano96@gmail.com>
2021-01-27 19:55:27 +0100

    category associata al post stampata nel backoffice(admin.posts.index)

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -8,12 +8,14 @@
                 <h1>Visualizzazione post id: {{ $post->id }}</h1>
             </div>
             <dl>
-                <dt>Titolo</dt>
+                <dt>Title</dt>
                 <dd>{{ $post->title }}</dd>
                 <dt>Slug</dt>
                 <dd>{{ $post->slug }}</dd>
-                <dt>Contenuto</dt>
+                <dt>Content</dt>
                 <dd>{{ $post->content }}</dd>
+                <dt>Category</dt>
+                <dd>{{$post->category ? $post->category->name : 'Non disponibile'}}</dd>
             </dl>
         </div>
     </div>
